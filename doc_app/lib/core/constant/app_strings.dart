8066c9ed749acc35358e13e6dc3b7e1f10ac247d
Abdelrahman Adel <aa5282243@gmail.com>
2025-02-28 11:48:11 +0200

    -ADD onBoarding Screen -ADD app fonts -ADD flutter_svg -ADD onBoarding images

---
+++ doc_app/lib/core/constant/app_strings.dart
@@ -0,0 +1,7 @@
+class AppStrings {
+  static const docDoc = 'Docdoc';
+  static const getStarted = 'Get Started';
+  static const manageAndS =
+      'Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.';
+  static const bestDoctor = '      Best Doctor\nAppointment App';
+}
---
+++ doc_app/lib/core/constant/fonts_strings.dart
@@ -0,0 +1,5 @@
+class FontsStrings {
+  static const interBold = 'inter_bold';
+  static const semiBold = 'semi_bold';
+  static const interRegular = 'inter_regular';
+}
