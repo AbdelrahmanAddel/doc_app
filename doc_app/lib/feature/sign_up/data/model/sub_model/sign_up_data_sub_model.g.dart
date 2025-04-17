5ef2404f195fae9651faedc018a3d54a087f26f7
Abdelrahman Adel <aa5282243@gmail.com>
2025-04-18 01:49:53 +0200

    - Signup API integrated and tested - Signup Cubit implemented and connected to the API - UI implementation is still pending

---
+++ doc_app/lib/feature/sign_up/data/model/sub_model/sign_up_data_sub_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sign_up_data_sub_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SignUpDataSubModel _$SignUpDataSubModelFromJson(Map<String, dynamic> json) =>
+    SignUpDataSubModel(
+      token: json['token'] as String?,
+      userName: json['username'] as String?,
+    );
+
+Map<String, dynamic> _$SignUpDataSubModelToJson(SignUpDataSubModel instance) =>
+    <String, dynamic>{
+      'token': instance.token,
+      'username': instance.userName,
+    };
---
+++ doc_app/lib/feature/sign_up/logic/repository/sign_up_repository.dart
@@ -0,0 +1,9 @@
+import 'package:doc_app/core/networking/api_result.dart';
+import 'package:doc_app/feature/sign_up/data/model/sign_up_request_model.dart';
+import 'package:doc_app/feature/sign_up/data/model/sign_up_responce_model.dart';
+
+abstract class SignUpRepository {
+  Future<ApiResult<SignUpResponceModel>> signUp(
+    SignUpRequestModel signUpRequestModel,
+  );
+}
