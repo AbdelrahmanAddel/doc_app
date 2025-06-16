4b632d2d0041656ade1f01c2f327ab98518076cc
Abdelrahman <Aa5282243@gmail.com>
2025-06-16 17:16:12 +0300

    feat: Add shimmer_animation dependency and update pubspec files feat: Enhance HomeCubit with filtering functionality for doctors by specialization fix: Improve HomeState to handle filtering results and errors

--- doc_app/lib/core/helper/extensions.dart
+++ doc_app/lib/core/helper/extensions.dart
@@ -30,3 +30,7 @@ extension Naviagtion on BuildContext {
 extension StringExtension on String? {
   bool isNullOrEmpty() => this == null || this == '';
 }
+
+extension ListExtension<T> on List<T>? {
+  bool isNullOrEmpty() => this == null || this!.isEmpty;
+}
